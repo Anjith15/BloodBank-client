c33500bc1a9e523e03591254c4c7504b34b1a61e
Anjith15 <anjithraogandla@gmail.com>
2025-05-21 21:59:13 +0530

    edit

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // API Configuration
-const API_BASE_URL = "https://bloodbank-server-v973.onrender.com";
+const API_BASE_URL = "https://bloodbank-server-v973.onrender.com:3000";
 
 // Export constants
 export { API_BASE_URL }; 
