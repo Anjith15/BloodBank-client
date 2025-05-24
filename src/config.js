a5b4c294cef71fe8f5962b747e78c7a354b71892
Anjith15 <anjithraogandla@gmail.com>
2025-05-24 12:28:03 +0530

    changed the config.js file

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // API Configuration
-// const API_BASE_URL = "https://bloodbank-server-v973.onrender.com";
+const API_BASE_URL = "https://bloodbank-server-v973.onrender.com";
 
-const API_BASE_URL = "http://localhost:3000";
+// const API_BASE_URL = "http://localhost:3000";
 // Export constants
 export { API_BASE_URL }; 
